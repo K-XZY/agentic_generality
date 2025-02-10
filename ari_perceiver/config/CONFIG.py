c45818b621c5bd2579f1b101b64a0aad4d442365
Arcanyas <nina.ariana@icloud.com>
2025-02-10 17:10:47 +0000

    Models file for easier acccessibility

--- ari_perceiver/config/CONFIG.py
+++ ari_perceiver/config/CONFIG.py
@@ -9,13 +9,13 @@ class Config:
         self.image_shape = (64, 64, 3)
         self.version = version
 
-        self.project_root = '/Users/ninastidham'
+        self.project_root = 'models'
         
         # For CNN trained on RICO Dataset
         if cnn_type == "cnn-rico":
-            self.CNN_PATH = pjoin(self.project_root, "models", "cnn-rico-1.h5")
+            self.CNN_PATH = pjoin(self.project_root, "modelpy3.8", "cnn-rico-1.h5")
             if self.version != "3.8":
-                self.CNN_PATH = pjoin(self.project_root, "edit_models", "converted_model.h5")
+                self.CNN_PATH = pjoin(self.project_root, "modelpy3.10+", "converted_model.h5")
 
             self.element_class = ['Button', 'CheckBox', 'Chronometer', 'EditText', 'ImageButton', 'ImageView',
                               'ProgressBar', 'RadioButton', 'RatingBar', 'SeekBar', 'Spinner', 'Switch',
